adaad66430af975e4a08cee6d25a172ed295d1da
root <root@kaa-debian.asia-northeast1-b.c.featuring.internal>
2020-04-01 13:03:17 +0900

    make it better

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 var express = require('express');
+var request = require('request');
+var moment  = require('moment');
+
 var app = express();
 var path = require('path');
 var server = require('http').createServer(app);
 var port = process.env.PORT || 4000;
 
+const websocket = require('ws')
+const wss = new websocket.Server({ port: 4001 });
 
-var express     = require("express");
-var request     = require('request');
-
-server.listen(port, () => {
-  console.log('Server listening at port %d', port);
-});
 
 app.use(express.json());
 app.use(express.urlencoded());
@@ -20,13 +19,81 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
+server.listen(port, () => { console.log('Server listening at port %d', port); });
+
 app.get('/', function(req, res){
   res.render('index');
 })
 
-app.post('/', function(req, res){
-  var body = req.body;
-  console.log(body);
+app.post('/scba', function(req, res){
+  var data = req.body;
+  console.log(data);
   res.send('200 Okay');
 })
 
+app.post('/hrm', function(req, res){
+  var data = req.body;
+  console.log(data);
+  res.send('200 Okay');
+})
+
+app.post('/accel', function(req, res){
+  var data = req.body;
+  console.log(data);
+  res.send('200 Okay');
+})
+
+app.post('/gyro', function(req, res){
+  var data = req.body;
+  console.log(data);
+  res.send('200 Okay');
+})
+
+app.post('/orient', function(req, res){
+  var data = req.body;
+  console.log(data);
+  res.send('200 Okay');
+})
+
+app.post('/magnetic', function(req, res){
+  var data = req.body;
+  console.log(data);
+  res.send('200 Okay');
+})
+
+app.get('/rtsp', function(req, res){
+  var data = req.body;
+  res.send('{ "stream": "rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov", "timestamp": ' + moment().unix() + ' }');
+})
+
+const timeout = 60000
+
+function cbReader(arg) {
+  console.log(moment().unix());
+  setTimeout(cbReader, timeout, null);
+}
+
+setTimeout(cbReader, timeout, null);
+
+var id = 0;
+var clients = [];
+
+function wsSendAll (message) {
+  for (var i=0; i<clients.length; i++) {
+    clients[i].send(message);
+  }
+}
+
+wss.on('connection', ws => {
+  clients.push (ws)
+  ws.send('Hello! Message From Server!!')
+
+  ws.on('message', message => {
+    console.log(`Received message => ${message}`)
+    wsSendAll (message);
+  })
+
+  ws.on('close', function close() {
+    console.log('disconnected');
+  });
+})
